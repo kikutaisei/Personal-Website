709405ec5a868602dc4458d7b9ca95d4f5581226
kikutaisei <0218kikutaisei@gmail.com>
2021-12-02 22:01:38 +0000

    Added home section contents and animations

--- code/script.js
+++ code/script.js
@@ -1,8 +1,9 @@
 // - - - SCRIPT FOR THE NAVBAR - - -
-gsap.from(".navbar", {duration: 1, y: "-100%", ease: "bounce"});
-gsap.from(".navbar-logo", {duration: 1, y: "-150%", ease: "elastic", delay: 0.8});
-gsap.from(".navbar-links li", {duration: 1, y: "-200%", ease: "elastic", delay: 1, stagger: 0.2});
-gsap.from(".menu-button", {duration: 1, y: "-250%", ease: "elastic", delay:"1"});
+const navbarTimeline = gsap.timeline({defaults: {duration: 0.5, y: "-250%", ease: "back"}});
+navbarTimeline
+  .from(".navbar", {duration: 1, y: "-100%", ease: "bounce"})
+  .from(".navbar-logo", {})
+  .from(".menu-button, .navbar-links li", {stagger: 0.2})
 
 const menuButton = document.getElementsByClassName('menu-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
@@ -10,3 +11,12 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 menuButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
 })
+
+// - - - SCRIPT FOR THE HOME SECTION - - -
+const homeSectionTimeline = gsap.timeline({defaults: {duration: 0.5, y: "50%", ease: "power 1", opacity: 0}});
+homeSectionTimeline
+  .from(".home-section-overlay", {duration: 1, y: "0%", ease: "power 1", opacity: 1})
+  .from(".home-section-titles h1", {})
+  .from(".home-section-titles h2", {})
+  .from(".home-section-titles a", {})
+  .from(".home-section-memoji", {x: "10%", y: "0%"})
